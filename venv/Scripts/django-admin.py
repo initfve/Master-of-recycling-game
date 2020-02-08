756f97d3221122ebb3fefb0f37b155c431f8cea6
xFreet <jaakubkowalczyk@gmail.com>
2020-02-08 20:52:51 +0100

    Start files, website template and graphics

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Kuba\PycharmProjects\Master_of_recycling\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
